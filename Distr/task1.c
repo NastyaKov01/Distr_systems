11a68288182b21941cea8643bd60f6b9aa74fc9b
Kovalenko Anastasia <s2180445@gse.cs.msu.ru>
2021-12-24 10:02:11 +0300

    Task1 completed

--- Distr/task1.c
+++ Distr/task1.c
@@ -12,16 +12,16 @@ int main(int argc, char **argv)
     // unsigned L = 8000;
     unsigned L = 80;
     // unsigned K = 100;
-    unsigned K = 10; 
-    unsigned block_size = L / (2 * K); 
+    // unsigned K = 10; 
+    // unsigned block_size = L / (2 * K); 
     int* message = malloc(L * sizeof(int));
     int paths[2][7] = {{0, 1, 2, 3, 7, 11, 15}, {0, 4, 8, 12, 13, 14, 15}};
     MPI_Status status;
+    MPI_Request req1, req2;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-    printf("Hello, I'm %d\n", rank);
     if (rank == 0) {
-        printf("Message creation\n");
+        printf("Created message in process %d:\n", rank);
         for (unsigned i = 0; i < L; i++) {
             message[i] = rand() % L;
             printf("%d ",  message[i]);
@@ -29,51 +29,54 @@ int main(int argc, char **argv)
         printf("\n");
     }
     
-    for (unsigned i = 0, tag = 0; i < 2; i++, tag++) {
-        for (unsigned j = 1; j < 7; j++) {
-            if (paths[i][j] == rank) {
-                unsigned shift = 0;
-                if (i == 1) {
-                    shift = K * block_size;
-                }
-                printf("Receive in %d\n", rank);
-                for (unsigned k = 0; k < K; k++) {
-                    MPI_Recv(message + k * block_size + shift, block_size, MPI_INT, paths[i][j - 1], tag, MPI_COMM_WORLD, &status);
-                    printf("%d ", k);
-                }
-                printf("\n");
-                break;
-            }
-        }
-    }
-    MPI_Barrier(MPI_COMM_WORLD);
-    // unsigned buff_size = L * sizeof(double) + MPI_BSEND_OVERHEAD;
-    // double* buff = malloc(buff_size);
-    // MPI_Buffer_attach(buff, buff_size);
-    for (unsigned i = 0, tag = 1; i < 2; i++, tag++) {
-        for (unsigned j = 0; j < 6; j++) {
-            if (paths[i][j] == rank) {
-                unsigned shift = 0;
-                if (i == 1) {
-                    shift = K * block_size;
-                }
-                printf("Send in %d\n", rank);
-                for (unsigned k = 0; k < K; k++) {
-                    MPI_Rsend(message + k * block_size + shift, block_size, MPI_INT, paths[i][j + 1], tag, MPI_COMM_WORLD);
-                    printf("%d ", k);
-                }
-                printf("\n");
-                break;
-            }
-        }
-    }
-    // MPI_Buffer_detach(buff, &buff_size);
-    // free(buff);
-    
     if (rank == 15) {
-        printf("Received message:\n");
+        MPI_Irecv(message, L / 2, MPI_INT, 11, 0, MPI_COMM_WORLD, &req1);
+        MPI_Irecv(message + L / 2, L / 2, MPI_INT, 14, 0, MPI_COMM_WORLD, &req2);
+        printf("\n");
+    } else {
+        for (unsigned i = 0, tag = 0; i < 2; i++, tag++) {
+            for (unsigned j = 1; j < 6; j++) {
+                if (paths[i][j] == rank) {
+                    unsigned shift = 0;
+                    if (i == 1) {
+                        shift = L / 2;
+                    }
+                    MPI_Irecv(message + shift, L / 2, MPI_INT, paths[i][j - 1], 0, MPI_COMM_WORLD, &req1);
+                    break;
+                }
+            }
+        }
+    }
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (rank == 15) {
+        MPI_Wait(&req1,  MPI_STATUS_IGNORE);
+        MPI_Wait(&req2,  MPI_STATUS_IGNORE);
+        printf("Received message in process %d:\n", rank);
         for (unsigned i = 0; i < L; i++) {
-            printf("%d \n",  message[i]);
+            printf("%d ",  message[i]);
+        }
+        printf("\n");
+    } else if (rank == 0) {
+        MPI_Irsend(message, L / 2, MPI_INT, 1, 0, MPI_COMM_WORLD, &req1);
+        MPI_Irsend(message + L / 2, L / 2, MPI_INT, 4, 0, MPI_COMM_WORLD, &req2);
+        MPI_Wait(&req1,  MPI_STATUS_IGNORE);
+        MPI_Wait(&req2,  MPI_STATUS_IGNORE);
+    } else {
+        
+        for (unsigned i = 0; i < 2; i++) {
+            for (unsigned j = 0; j < 6; j++) {
+                if (paths[i][j] == rank) {
+                    MPI_Wait(&req1,  MPI_STATUS_IGNORE);
+                    unsigned shift = 0;
+                    if (i == 1) {
+                        shift = L / 2;
+                    }
+                    MPI_Rsend(message + shift, L / 2, MPI_INT, paths[i][j + 1], 0, MPI_COMM_WORLD);
+                    break;
+                }
+            }
         }
     }
 
