8ec6b3083e6aff2a6643622cdb4757100ad07380
Kovalenko Anastasia <s2180445@gse.cs.msu.ru>
2021-12-27 11:05:10 +0300

    Control points added

--- Distr/task2.c
+++ Distr/task2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <signal.h>
 #include <mpi.h>
 #include <mpi-ext.h>
 
 MPI_Comm main_comm = MPI_COMM_WORLD;
-int rank, num_procs;
+int rank, num_procs, dead_rank, points_num = 4;
 
 static void verbose_errhandler(MPI_Comm* pcomm, int* perr, ...) {
-    // MPI_Comm comm = *pcomm;
     int err = *perr;
     char errstr[MPI_MAX_ERROR_STRING];
     int i, size, nf=0, len, eclass;
@@ -40,11 +40,8 @@ static void verbose_errhandler(MPI_Comm* pcomm, int* perr, ...) {
     for(i = 0; i < nf; i++)
         printf("%d ", ranks_gc[i]);
     printf("}\n");
-    // MPIX_Comm_revoke(comm);
-    // MPIX_Comm_shrink(comm, &new_comm);
+    dead_rank = ranks_gc[0];
     free(ranks_gf); free(ranks_gc);
-    // int spawn_error;
-    // MPI_Comm_spawn(argv[0], argv[1], 1, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &main_comm, &spawn_error);
     MPIX_Comm_shrink(*pcomm, &main_comm);
     MPI_Comm_rank(main_comm, &rank);
     MPI_Comm_size(main_comm, &num_procs);
@@ -55,21 +52,55 @@ double fun(double x)
     return x / (x * x * x + x * x + 5 * x + 1);
 }
 
-double integral(double a, double b, int n, int myrank, int num_procs)
+double integral(double a, double b, int n, int myrank)
 {
     double res = 0; 
     double h = (b - a) / (2 * n);
     double x1, x2;
+    int cnt = 1, part = n / num_procs / points_num;
+    FILE *fw;
+    char filename[15];
     for (int i = myrank; i <= n; i += num_procs) {
         x1 = a + (2 * i - 1) * h;
         x2 = a + 2 * h * i;
         res += 4 * fun(x1);
         res += 2 * fun(x2);
+        if (i == myrank + cnt * part * num_procs && cnt != points_num) {
+            sprintf(filename, "%d_%d", rank, cnt);
+            fw = fopen(filename, "w");
+            fprintf(fw, "%lf", res);
+            cnt++;
+            fclose(fw);
+        }
+        if (rank == num_procs - 1 && i > myrank + 2 * part * num_procs) {
+            raise(SIGKILL);
+        }
     }
+    sprintf(filename, "%d_%d", rank, cnt);
+    fw = fopen(filename, "w");
+    fprintf(fw, "%lf", res);
+    fclose(fw);
     res += fun(b - h);
     res += fun(a) + fun(b);
     res *= (h / 3);
     return res;
+}
+
+double recovery_integral(double a, double b, int n, int myrank, double cur_res, int cnt)
+{
+    double h = (b - a) / (2 * n);
+    double x1, x2;
+    int part = n / num_procs / points_num;
+    for (int i = myrank + (cnt * part + 1) * num_procs; i <= n; i += num_procs) {
+        x1 = a + (2 * i - 1) * h;
+        x2 = a + 2 * h * i;
+        cur_res += 4 * fun(x1);
+        cur_res += 2 * fun(x2);
+    }
+    cur_res += fun(b - h);
+    cur_res += fun(a) + fun(b);
+    cur_res *= (h / 3);
+    return cur_res;
 }
 
 int main(int argc, char **argv)
@@ -84,7 +115,7 @@ int main(int argc, char **argv)
     
     MPI_Comm_size(main_comm, &num_procs);
     MPI_Comm_rank(main_comm, &rank);
-    
+
     MPI_Comm_create_errhandler(verbose_errhandler, &errh);
     MPI_Comm_set_errhandler(main_comm, errh);
 
@@ -92,16 +123,28 @@ int main(int argc, char **argv)
     if (rank == 0) {
         start = MPI_Wtime();
     }
-
-    if (rank == num_procs - 1) {
-        raise(SIGKILL);
-    }
     MPI_Barrier(main_comm);
     MPI_Bcast(&n, 1, MPI_INT, 0, main_comm);
-    double cur_sum = integral(a, b, n, rank, num_procs);
+    double cur_sum = integral(a, b, n, rank);
+    MPI_Barrier(main_comm);
     MPI_Reduce(&cur_sum, &result, 1, MPI_DOUBLE, MPI_SUM, 0, main_comm);
 
     if (rank == 0) {
+        FILE *fr;
+        int cnt = points_num;
+        char filename[15];
+        double res;
+        sprintf(filename, "%d_%d", dead_rank, cnt);
+        fr = fopen(filename, "r");
+        while (!fr) {
+            cnt--;
+            sprintf(filename, "%d_%d", dead_rank, cnt);
+            fr = fopen(filename, "r");
+        }
+        fscanf(fr, "%lf", &res);
+        fclose(fr);
+        res = recovery_integral(a, b, n, dead_rank, res, cnt);
+        result += res;
         printf("Result is %lf\n", result);
         finish = MPI_Wtime();
         printf("Time passed: %lf\n", finish - start);
